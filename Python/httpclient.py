a057eb064c7e25004a2bfadbae6f0db188bfdee2
tomsnode <tomcatalano.0@gmail.com>
2020-07-08 21:40:59 -0600

    Delete httpclient.py

--- Python/httpclient.py
+++
@@ -1,7 +0,0 @@
-import httplib
- 
-connection = httplib.HTTPSConnection('www.conceptslearningmachine.com',cert_file='myCert.PEM')
-connection.request('GET','/index.html')
-response = connection.getresponse()
-data = response.read()
- 
