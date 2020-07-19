75303f854803c749422b4fc12cea65bc2443588c
tomsnode <tomcatalano.0@gmail.com>
2020-07-19 14:17:03 -0600

    Create ASMtemp.asm

---
+++ ASMx/ASMx/programming language Assembly/examples/ASMtemp.asm
@@ -0,0 +1,10 @@
+asm // asm-qualifiers ( AssemblerTemplate 
+                 : OutputOperands 
+                 [ : InputOperands
+                 [ : Clobbers ] ])
+
+asm // asm-qualifiers ( AssemblerTemplate 
+                      : 
+                      : InputOperands
+                      : Clobbers
+                      : GotoLabels)
